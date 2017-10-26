3df37d993b8d9137f1c8880b310265cd28290be2
konradtab <tabiskonrad@gmail.com>
2017-10-26 21:30:14 +0200

    nowe sprawozdanie

--- scenariusz_1/Main.java
+++ scenariusz_1/Main.java
@@ -2,55 +2,40 @@
  * Created by Dell on 14.10.2017.
  */
 public class Main {
-    public static void main(String[] args)
-    {
-        Perceptron perceptron=new Perceptron();
+    public static void main(String[] args) {
+        Perceptron perceptron = new Perceptron();
 
-        int a=4,b=4,c=4,d=4,s=a+b+c+d;
-        int bias=1;
-        double learning_factor=0.1;
+        int bias = 1;
+        int ilosc_epok=8;
+        double learning_factor = 0.15;
 
         //tablice wejściowe
-        int[] x1 ={0,1,0,1};
-        int[] x2 ={0,0,1,1};
+        int[] x1 = {0, 1, 0, 1};
+        int[] x2 = {0, 0, 1, 1};
 
         //tabela wynikowa dla AND
-        int[] y={0,0,0,1};
+        int[] y = {0, 0, 0, 1};
 
-        //j-ilość wykonanych pętli na konkretnym zestawie danych
-        //pętla dla pierwszej danej [0,0|0]
-        for ( int i = 0,j=0; j < a; j++ ) {
-            perceptron.learn_perceptron( bias, x1[i], x2[i], y[i], learning_factor );
-        }
-        //pętla dla drugiej danej [1,0|0]
-        for ( int i = 1,j=0; j < b; j++ ) {
-            perceptron.learn_perceptron( bias, x1[i], x2[i], y[i], learning_factor );
-        }
-        //pętla dla trzeciej danej [0,1|0]
-        for ( int i = 2,j=0; j < c; j++ ) {
-            perceptron.learn_perceptron( bias, x1[i], x2[i], y[i], learning_factor );
-        }
-        //pętla dla czwartej danej [1,1|1]
-        for ( int i = 1,j=0; j < d; j++ ) {
-            perceptron.learn_perceptron( bias, x1[i], x2[i], y[i], learning_factor );
-        }
-
+        //pętle uczące perceptron z wyświetlaniem wyników po każdej epoce
         System.out.println("Wyniki uczenia perceptonu funkcji AND\n");
-        System.out.println("Przeprowadzono " + s + " prób uczenia się\nDla 0,0 przeprowadzono " +a+ " próby\nDla 1,0 przeprowadzono " +b+
-        " próby\nDla 0,1 przeprowadzono " +c+ " próby\nDla 1,1 przeprowadzono "+d+ " próby\n");
+        for(int i =0;i<ilosc_epok;i++){
+            for(int j=0;j<4;j++)
+            {
+                perceptron.learn_perceptron(bias,x1[j],x2[j],y[j],learning_factor);
+            }
+            //Wyświetlanie wyników po każdej epoce
+            System.out.println("Epoka numer: "+ i + "\n");
+            System.out.println("x1="+x1[0]+" x2="+x2[0]+" y="+y[0]+" wynik = "+ perceptron.process(bias,x1[0],x2[0])+"\n");
+            System.out.println("x1="+x1[1]+" x2="+x2[1]+" y="+y[1]+" wynik = "+ perceptron.process(bias,x1[1],x2[1])+"\n");
+            System.out.println("x1="+x1[2]+" x2="+x2[2]+" y="+y[2]+" wynik = "+ perceptron.process(bias,x1[2],x2[2])+"\n");
+            System.out.println("x1="+x1[3]+" x2="+x2[3]+" y="+y[3]+" wynik = "+ perceptron.process(bias,x1[3],x2[3])+"\n\n");
+        }
 
-        System.out.println( "WAGI:" );
-        System.out.println( "W0 = " + perceptron.weight0 );
-        System.out.println( "W1 = " + perceptron.weight1 );
-        System.out.println( "W2 = " + perceptron.weight2 );
-
-        System.out.println("\nWynik dla x1=0 i x2=0");
-        System.out.println( perceptron.process( bias, 0, 0 ) );
-        System.out.println("\nWynik dla x1=0 i x2=1");
-        System.out.println( perceptron.process( bias, 0, 1 ) );
-        System.out.println("\nWynik dla x1=1 i x2=0");
-        System.out.println( perceptron.process( bias, 1, 0 ) );
-        System.out.println("\nWynik dla x1=1 i x2=1");
-        System.out.println( perceptron.process( bias, 1, 1 ) );
+        //Wyświetlenie wag końcowych
+        System.out.println("WAGI:");
+        System.out.println("W0 = " + perceptron.weight0);
+        System.out.println("W1 = " + perceptron.weight1);
+        System.out.println("W2 = " + perceptron.weight2);
     }
-}
+    }
+
--- scenariusz_1/Perceptron.java
+++ scenariusz_1/Perceptron.java
@@ -3,21 +3,22 @@ import java.util.Random;
 /**
  * Created by Dell on 13.10.2017.
  */
-
+//klasa perceptronu
 public class Perceptron {
     Random generator =new Random();
 
     //wagi poczatkowe
-    //1)ustawione randomowo
+    //ustawione randomowo
     public double weight0=generator.nextDouble(),weight1=generator.nextDouble() ,weight2=generator.nextDouble();
-    //2)ustawione na wartość stałą
-    //public double weight0=0.5,weight1=0.5,weight2=0.5;
 
+    //funkcja progowa
     public int perceptronOut (double perceptronOut)
     {
         if(perceptronOut<0)return 0;
         else return 1;
     }
+
+    //funkcja sumująca iloczyny danych wejściowych i odpowiedających im wag
     public int process ( int x0, int x1, int x2 ) {
         double y_p = x0 * weight0 + x1 * weight1 + x2 * weight2;
         return perceptronOut( y_p );
@@ -26,10 +27,9 @@ public class Perceptron {
     //algorytm uczenia perceptronu
     public void learn_perceptron(int x0, int x1, int x2, double y, double  learning_factor)
     {
-        //double perceptronOut = x0 * weight0 + x1 * weight1 + x2 * weight2;
-        //perceptronOut = perceptronOut(perceptronOut);
         double percOut=process(x0,x1,x2);
         weight0 += ( y - percOut ) * learning_factor * x0;
+        weight1 += ( y - percOut ) * learning_factor * x1;
+        weight2 += ( y - percOut ) * learning_factor * x2;
     }
-
 }
